5bd8b9a21c1b4978fc42027518e979070a8208c5
Jonathan <emeraldusk@gmail.com>
2024-02-01 19:38:26 -0300

    feat(sherlock): notes pwned

--- README.md
+++ README.md
@@ -2,7 +2,7 @@ Machines from hackthebox 📗
 
 ![wallpaper](https://wallpapercave.com/wp/wp5140187.jpg)
 
-They are inside the `machines` folder. Each machine has its own folder with the following structure:
+They are inside the `machines` or `sherlocks` folder. Each one has its own folder with the following structure:
 
 ```
 README.md
--- scripts/create_machine.sh
+++ scripts/create_machine.sh
@@ -1,13 +1,26 @@
 #!/bin/bash
 
 folder_name=$1
+is_machine=$2
 
-if [ -z $folder_name ]; then
+create_directories_and_files() {
+  local base_path=$1
+
+  mkdir -p "$base_path/$folder_name"
+  mkdir -p "$base_path/$folder_name/assets/"
+  mkdir -p "$base_path/$folder_name/utils/"
+  touch "$base_path/$folder_name/README.md"
+}
+
+if [ -z "$folder_name" ]; then
   echo "Please provide a folder name"
   exit 1
 fi
 
-mkdir -p machines/$folder_name
-mkdir -p machines/$folder_name/assets/
-mkdir -p machines/$folder_name/utils/
-touch machines/$folder_name/README.md
+if [ -z "$is_machine" ]; then
+  create_directories_and_files "sherlocks"
+  exit 0
+fi
+  
+create_directories_and_files "machines"
+echo "Done 🥳"
